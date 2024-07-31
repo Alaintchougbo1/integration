dd82453caae69d6e361ba18b91d501f763dcd26e
Alaintchougbo1 <alain.tchougbo@epitech.eu>
2024-07-31 16:38:43 +0100

    Installation du projet

---
+++ integrationYowl/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style scoped></style>
---
+++ integrationYowl/src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+  <h1 class="bg-orange-800">Hello world</h1>
+</template>
+
+<style scoped></style>
